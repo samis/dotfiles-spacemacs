9b6e317f65c66a6d5d701165c59668f05bec6429
Samuel Hodgkins <samuel.hodgkins@sky.com>
2016-08-24 00:53:12 +0100

    Move out Customize to own file.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -250,21 +250,6 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (cua-mode t)
+  (setq custom-file "~/.spacemacs.d/customize.el")
+  (load custom-file)
 )
-
-;; Do not write anything past this comment. This is where Emacs will
-;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(delete-selection-mode nil)
- '(paradox-github-token " 0b5c5380ba1a33436593ec6d7278c3e996bc3a90"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
